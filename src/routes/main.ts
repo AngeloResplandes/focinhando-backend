654ad3bdf236afb5776168537eff5202a7bf2535
AngeloResplandes <angeloodrigues@gmail.com>
2025-10-19 12:31:55 -0300

    feat: update e delete publication

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -39,6 +39,8 @@ routes.use("/pets", petRouter);
 const publicationRouter = Router();
 publicationRouter.post("/register", ...adminOnly, publicationController.postPublication);
 publicationRouter.get("/all-publications", publicationController.getPublications);
+publicationRouter.put("/:id", ...adminOnly, publicationController.putPublication);
+publicationRouter.delete("/:id", ...adminOnly, publicationController.deletePublicationById);
 routes.use("/publication", publicationRouter);
 
 const userRouter = Router();
